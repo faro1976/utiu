9bcc8a1578c05537fecc1127814b65c2873f50ca
robertofavaroni <robertofavaroni@172.22.246.2>
2019-02-14 12:18:15 +0100

    implemented dynamic sites loading from relevant brca genes

--- bioinf/BioInfPrj/src/main/scala/it/utiu/bioinf/Runner.scala
+++ bioinf/BioInfPrj/src/main/scala/it/utiu/bioinf/Runner.scala
@@ -18,6 +18,10 @@ import org.apache.spark.sql.{ Row, SparkSession }
 import org.apache.spark.sql.types.{ DoubleType, StringType, StructField, StructType }
 import org.apache.spark.sql.functions
 import scala.collection.mutable.Map
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.log4j.lf5.LogLevel
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 
 
 object Runner {
@@ -28,6 +32,7 @@ object Runner {
       .master("local")
       .getOrCreate()
     val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
     
      println("post spark context init!!")
 
@@ -36,7 +41,10 @@ object Runner {
 
     val clinics =Array("manually_curated__tissue_status", "biospecimen__shared__patient_id")
     val genesBRCA =Array("A2M", "ABCB1", "ABCC1", "ACOT7", "ACOX2","ADAMTS16", "ADAMTS17", "ADORA2A", "AQP1", "CA12", "CDCP1", "CREB3L1", "CRYAB", "FGF1","IL11RA", "INHBA", "ITIH5", "KIF26B", "LRRC3B", "MEG3", "MUC1", "PRKD1", "SDPR")
-    val sitesBRCA = Array("cg12417807","cg11139127","cg27166707")
+//    val sitesBRCA = Array("cg12417807","cg11139127","cg27166707")
+    
+    
+    
     def parsePatient(t: (String, String)): Row = {
       println("parse!!")
       val patientKV = Map[String,String]() 
@@ -45,136 +53,148 @@ object Runner {
         val kv = line.split("\t")
         patientKV += (kv(0) -> kv(1))        
       })
-      Row(patientKV.get("manually_curated__opengdc_id").get, patientKV.get("biospecimen__shared__patient_id").get, patientKV.get("manually_curated__tissue_status").get)
+      var status:Double = Double.NaN
+      patientKV.get("manually_curated__tissue_status").get match {
+        case "tumoral" => status = 1.0 
+         case "normal" => status = 0.0
+         case  _ => throw new RuntimeException("tissue status not recognized")
+      }
+      val r = Row(patientKV.get("manually_curated__opengdc_id").get, patientKV.get("biospecimen__shared__patient_id").get, status)
+      println(r)
+      r
     }
     
-    def parseSample(t: (String, String)): Row = {
-      val CpGKV = Map[String,String]()
-      val lines = t._2.split("\n")
-      lines.map(line=>{
-        val params = line.split("\t")
-        //verifica gene
-//        if (genesBRCA.contains(params(6))) {
-        if (sitesBRCA.contains(params(4))) {        
-          CpGKV += (params(4) -> params(5))
-        }        
-      })
-      Row(t._1, sitesBRCA.map(CpGKV.getOrElse(_, "")))
-    }
+
     
-        /*.filter(_(6) != "?")*/
-        /*.map(_.drop(1))*/
-        //        .map(t=>{
-        //          t(1) = if (t(1)=="tumoral") "1" else "0"  //definisco la classe di tipo Long
-        //          t.drop(0)                                 //elimino campo id campione
-        //          t.map(_.toDouble)                         //trasformo campi da string a double
-        //        })
-//        .map(
-//          line => Row(if (line(1) == "tumoral") 1.0 else 0.0, line(2).toDouble, line(3).toDouble, line(4).toDouble, line(5).toDouble))
-//    }
-//
-//    def parseSample(rdd: RDD[String]): RDD[Row] = {
-//      rdd.map(_.split(";"))
-//        /*.filter(_(6) != "?")*/
-//        /*.map(_.drop(1))*/
-//        //        .map(t=>{
-//        //          t(1) = if (t(1)=="tumoral") "1" else "0"  //definisco la classe di tipo Long
-//        //          t.drop(0)                                 //elimino campo id campione
-//        //          t.map(_.toDouble)                         //trasformo campi da string a double
-//        //        })
-//        .map(
-//          line => Row(if (line(1) == "tumoral") 1.0 else 0.0, line(2).toDouble, line(3).toDouble, line(4).toDouble, line(5).toDouble))
-//    }
-    
-    
+
         val schemaPatient = new StructType()
       .add(StructField("manually_curated__opengdc_id", StringType, false))
       .add(StructField("biospecimen__shared__patient_id", StringType, false))
-      .add(StructField("manually_curated__tissue_status", StringType, false))
+      .add(StructField("label", DoubleType, false))
 
-        val schemaSample = new StructType()
-      .add(StructField("manually_curated__opengdc_id", StringType, false))
-      
-      sitesBRCA.map(s=>schemaSample.add(StructField(s, DoubleType, false)))
-      
       
       
     //uso wholeTextFiles per poter leggere i dati in maniera autocontenuta ad ogni iterazione di map
       println("whole!!")
     val rddPatient = sc.wholeTextFiles("/Users/robertofavaroni/UniNettuno/courses/Big Data applicati alla bioinformatica/project/dataset/tcga-brca/*.meta").map(parsePatient)
-    val dfPatient = spark.createDataFrame(rddPatient, schemaPatient)
+    val dfPatient = spark.createDataFrame(rddPatient, schemaPatient).cache
     dfPatient.show()
     
+    //TODO ROB: un modo più elegante e semplice per sapere associazione siti->gene??
+    val rddSites = sc.textFile("/Users/robertofavaroni/UniNettuno/courses/Big Data applicati alla bioinformatica/project/dataset/tcga-brca/*.bed").map(r=>{
+      val vals = r.split("\t")
+      var ret = ""
+      if (genesBRCA.contains(vals(6))) {
+        ret = vals(4)
+      }
+      ret
+    }).filter(_!="").distinct()
+    //TODO ROB: fare dimensionality (features) reduction!
+    val sitesBRCA = rddSites.take(10)
+    println("sitesBRCA length: " + sitesBRCA.length)
+    
+    def parseSample(t: (String, String)): Row = {
+      val CpGKV = Map[String,Double]()
+      val lines = t._2.split("\n")
+      lines.foreach(line=>{
+        val params = line.split("\t")
+        //verifica sito
+        if (sitesBRCA.contains(params(4))) {        
+          CpGKV += (params(4) -> params(5).toDouble)
+        }        
+      }) 
+      val filename = t._1.split("/")
+//      val r = Row(filename(filename.size-1).split("\\.")(0), CpGKV.getOrElse(sitesBRCA(0), null),CpGKV.getOrElse(sitesBRCA(1), null),CpGKV.getOrElse(sitesBRCA(2), null))
+      val strs = List[String](filename(filename.size-1).split("\\.")(0)) 
+      val values = ListBuffer[Double]()
+      sitesBRCA.foreach(s=>values.append(CpGKV.getOrElse(s, Double.NaN)))
+      val r = Row.fromSeq((strs ::: values.toList).toSeq)
+      println("row sample: "+r)
+      r
+    }
+        
+//        val schemaSample = new StructType()
+        val arrSchemaSample = ArrayBuffer[StructField]()
+//      .add(StructField("manually_curated__opengdc_id", StringType, false))
+//      .add(StructField(sitesBRCA(0), DoubleType, true))
+//      .add(StructField(sitesBRCA(1), DoubleType, true))
+//      .add(StructField(sitesBRCA(2), DoubleType, true))      
+//      sitesBRCA.foreach(s=>schemaSample.add(StructField(s, DoubleType, false)))
+        arrSchemaSample.append(StructField("manually_curated__opengdc_id", StringType, false))
+        sitesBRCA.foreach(s=>arrSchemaSample.append(StructField(s, DoubleType, false)))
+        val schemaSample = new StructType(arrSchemaSample.toArray)
+      println(schemaSample)
+     
+      
+      
+      
+        
     val rddSample = sc.wholeTextFiles("/Users/robertofavaroni/UniNettuno/courses/Big Data applicati alla bioinformatica/project/dataset/tcga-brca/*.bed").map(parseSample)
-    val dfSample = spark.createDataFrame(rddSample, schemaSample)
+    val dfSample = spark.createDataFrame(rddSample, schemaSample).cache
     dfSample.show()
+
+    val dfExtended = dfSample.join(dfPatient, "manually_curated__opengdc_id")
+    dfExtended.show()
+    
+    //set the input and output column names**
+    val assembler = new VectorAssembler().setInputCols(sitesBRCA).setOutputCol("features")
+    //return a dataframe with all of the  feature columns in  a vector column**
+    val dfFeat = assembler.transform(dfExtended)
+    // the transform method produced a new column: features.
+    dfFeat.show
+
+    //  Create a label column with the StringIndexer
+//    val labelIndexer = new StringIndexer().setInputCol("status").setOutputCol("label")
+//    val dfLabel = labelIndexer.fit(dfFeat).transform(dfFeat)
+    val dfLabel = dfFeat
+    // the  transform method produced a new column: label.
+    dfLabel.show
+
+    //  split the dataframe into training and test data
+    val splitSeed = 5043
+    val Array(trainingData, testData) = dfLabel.randomSplit(Array(0.7, 0.3), splitSeed)
+
+    
+   //QUI FARE PROVE PER DIFFERENTI CLASSIFICATORI!! 
+//Classification Models in MLlib: Spark has several models available for performing binary and multiclass classification out of the box. The following models are available for classification in Spark:
+//-Logistic regression
+//-Decision trees
+//-Random forests
+//-Gradient-boosted trees
     
     
-//    val dfPatient = parsePatient(rddPatient)
-//    //    val obsRDD = parseRDD(rdd).map(parseObs)
-//    val obsRDD = parseRDD(rdd)
-//
-//    //val rowsRdd: RDD[Row] = sc.parallelize(
-//    //  Seq(
-//    //    Row("first", 2.0, 7.0),
-//    //    Row("second", 3.5, 2.5),
-//    //    Row("third", 7.0, 5.9)
-//    //  )
-//    //)
-//    val schema = new StructType()
-//      .add(StructField("class", DoubleType, false))
-//      .add(StructField("cg13869341", DoubleType, true))
-//      .add(StructField("cg14008030", DoubleType, true))
-//      .add(StructField("cg06937459", DoubleType, true))
-//      .add(StructField("cg16355945", DoubleType, true))
-//      
-//      
-//
-//    //val fields = schemaString.split(" ")
-//    //  .map(fieldName => StructField(fieldName, StringType, nullable = true))
-//    //val schema = StructType(fields)
-//    val df = spark.createDataFrame(obsRDD, schema)
-//
-//    df.show()
-//    df.describe().show()
-//    df.printSchema()
-//
-//    //    val obsDF = spark.createDataFrame(rowsRdd)
-//    //    val obsDF = obsRDD.toDF().cache()
-//    //obsDF.registerTempTable("obs")
-//
-//    //define features
-//    val featureCols = Array("cg06937459", "cg14008030")
-//
-//    //set the input and output column names**
-//    val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
-//    //return a dataframe with all of the  feature columns in  a vector column**
-//    val df2 = assembler.transform(df)
-//    // the transform method produced a new column: features.
-//    df2.show
-//
-//    //  Create a label column with the StringIndexer
-//    val labelIndexer = new StringIndexer().setInputCol("class").setOutputCol("label")
-//    val df3 = labelIndexer.fit(df2).transform(df2)
-//    // the  transform method produced a new column: label.
-//    df3.show
-//
-//    //  split the dataframe into training and test data
-//    val splitSeed = 5043
-//    val Array(trainingData, testData) = df3.randomSplit(Array(0.7, 0.3), splitSeed)
-//
-//    // create the classifier,  set parameters for training
-//    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+    //LOGISTIC REGRESSION CLASSIFIER
+    // create the classifier,  set parameters for training
+    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+    //  use logistic regression to train (fit) the model with the training data**
+    val model = lr.fit(trainingData)
+
+    // Print the coefficients and intercept for logistic regression
+    println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
+
+    // run the  model on test features to get predictions
+    val predictions = model.transform(testData)
+    //As you can see, the previous model transform produced a new columns: rawPrediction, probablity and prediction.
+    predictions.show(100)
+    
+   //A common metric used for logistic regression is area under the ROC curve (AUC). We can use the BinaryClasssificationEvaluator to obtain the AUC
+  // create an Evaluator for binary classification, which expects two input columns: rawPrediction and label.
+  val evaluator = new BinaryClassificationEvaluator().setLabelCol("label").setRawPredictionCol("rawPrediction").setMetricName("areaUnderROC")
+ // Evaluates predictions and returns a scalar metric areaUnderROC(larger is better).
+  val accuracy = evaluator.evaluate(predictions)
+  println("accuracy: " + accuracy)    
+      
+//    //DECISION TREES CLASSIFIER
+//    val classifierDT = new DecisionTreeClassifier()
 //    //  use logistic regression to train (fit) the model with the training data**
-//    val model = lr.fit(trainingData)
-//
-//    // Print the coefficients and intercept for logistic regression
-//    println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
+//    val modelDT = classifierDT.fit(trainingData)
 //
 //    // run the  model on test features to get predictions
-//    val predictions = model.transform(testData)
+//    val predictionsDT = modelDT.transform(testData)
 //    //As you can see, the previous model transform produced a new columns: rawPrediction, probablity and prediction.
 //    predictions.show
+    
+    
     spark.stop()
   }
 }
