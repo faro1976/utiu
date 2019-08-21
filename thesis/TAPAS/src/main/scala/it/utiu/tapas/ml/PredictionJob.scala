bceb47b43a240d3431753e5eb76529b6e1724b06
RF <faro1976@a.a>
2019-08-21 08:33:52 +0200

    linear regression without memory pool

--- thesis/TAPAS/src/main/scala/it/utiu/tapas/ml/PredictionJob.scala
+++ thesis/TAPAS/src/main/scala/it/utiu/tapas/ml/PredictionJob.scala
@@ -5,14 +5,26 @@ import java.io.FileWriter
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.Date
-
 import scala.collection.JavaConversions.seqAsJavaList
-
+import scala.collection.mutable.ListBuffer
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
-
-import au.com.bytecode.opencsv.CSVWriter
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+import com.google.gson.JsonParser
+import au.com.bytecode.opencsv.CSVWriter
+import org.apache.spark.sql.types.Metadata
+import org.apache.spark.sql.types.BooleanType
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.functions.col
+
+
 
 
 object PredictionJob {
@@ -20,9 +32,9 @@ object PredictionJob {
   val PATH = "hdfs://localhost:9000/bitcoin/"  //HDFS path
 //  val sdfm = new SimpleDateFormat("yyMMddhhmm")
   val decF = new DecimalFormat("#.####");  
-  val sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+  val sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val sdf2 = new SimpleDateFormat("yyyy-MM-dd")
-  val sdf3 = new SimpleDateFormat("hh")
+  val sdf3 = new SimpleDateFormat("HH")
   
   
   
@@ -35,11 +47,12 @@ object PredictionJob {
       .config(conf)
       .getOrCreate()
     val sc = spark.sparkContext
-    sc.setLogLevel("DEBUG")
+    sc.setLogLevel("INFO")
 
     
     //popolamento RDD con dati delle transazioni Bitcoin in formato CSV     
-    val rddTxsRaw = sc.textFile(PATH+"transactions/*.csv")
+//    val rddTxsRaw = sc.textFile(PATH+"transactions/*.csv")
+    val rddTxsRaw = sc.textFile(PATH+"transactions/rob.csv")
     val rddTxsHead = rddTxsRaw.first()
     val rddTxs = rddTxsRaw.filter(row => row != rddTxsHead).filter(line=>line.split(",")(6).length()>0)
       .map(line=>{
@@ -71,82 +84,82 @@ object PredictionJob {
     
     
     //join per data degli RDDs statistiche transazioni giornaliere e quotazioni Bitcoin giornaliere
-    //resituisce RDD: _id,fee,h,segwit,size,t,tfs,vol,hash,confTime,hourOfDay,price 
+    //resituisce RDD: fee,segwit,size,t,tfs,vol,hash,confTime,hourOfDay,price 
     val rddJoined = rddTxs.join(rddDailyPrice)
-        .map(t=>(t._2._1._1,t._2._1._2,t._2._1._3,t._2._1._4,t._2._1._5,t._2._1._6,t._2._1._7,t._2._1._8,t._2._1._9,t._2._1._10,t._2._1._11,t._2._2))
+        .map(t=>Row(t._2._1._2.toDouble,t._2._1._4.toBoolean,t._2._1._5.toInt,t._2._1._6.toLong,t._2._1._7.toLong,t._2._1._8.toDouble,t._2._1._9.toString(),t._2._1._10.toLong,t._2._1._11.toLong,t._2._2.toDouble))
     
     rddJoined.first()
-//                
-//    
-//    //definzione schema dataframe finale    
-//    val schema = new StructType()
-//      .add(StructField("date", StringType, false, Metadata.empty))
-//      .add(StructField("price", DoubleType, false, Metadata.empty))
-//      .add(StructField("total_transactions", DoubleType, false, Metadata.empty))
-//      .add(StructField("total_amount", DoubleType, false, Metadata.empty))
-//      .add(StructField("average_amount", DoubleType, false, Metadata.empty))
-//      .add(StructField("average_fee", DoubleType, false, Metadata.empty))      
-//    
-//      
-//    //creazione e popolamento dataframe con esclusione righe contenenti campi NaN  
-//    val df = spark.createDataFrame(rddJoined, schema).na.drop()
-//    df.show()
-//    
-//    
-//    //creazione CSV statistiche e quotazioni raggruppate per giorno
-//    var lstStats = new ListBuffer[Array[String]]()
-//    //header
-//    lstStats.append(Array("date", "price", "total_transactions", "total_amount", "average_amount", "average_fee"))    
-//    df.sort("date").collect().foreach(r=>lstStats.append(Array(r.get(0).toString(),decF.format(r.get(1)),r.get(2).toString(),r.get(3).toString(),decF.format(r.get(4)),decF.format(r.get(5)))))    
-//    writeCSV("web/csv/dailyStats.csv", lstStats.toList)
-//    
-//    
-//    //algoritmo di machine learning supervisionato per predizione quotazione Bitcoin mediante regressione lineare
-//    //definizione vettore di features
-//    val assembler = new VectorAssembler().setInputCols(Array("total_transactions","total_amount","average_amount","average_fee")).setOutputCol("features")
-//    val dfML = assembler.transform(df).cache()
-//    
-//    
-//    //definizione training e test set
-//    val Array(training, test) = dfML.randomSplit(Array(0.7, 0.3), 123)
-//    println("training count:"+training.count())
-//    println("test count:"+test.count())
-//
-//    
-//    //creazione modello
-//    val lr = new LinearRegression()
-//      .setMaxIter(10)
-//      .setRegParam(0.3)
-//      .setElasticNetParam(0.8)
-//      .setFeaturesCol("features")
-//      .setPredictionCol("predictedPrice")
-//      .setLabelCol("price")
-//
-//      
-//    //apprendimento modello su training set
-//    val lrModel = lr.fit(training)
-//
-//    
-//    //stampa del coefficiente angolare e intercetta della funzione individuata
-//    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-//
-//    
-//    //stampa statistiche
-//    val trainingSummary = lrModel.summary
-//    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-//    println(s"r2: ${trainingSummary.r2}")    
-//    
-//    
-//    //applicazione modello su dati di test e valorizzazione predittori
-//    val predictions = lrModel.transform(test)
+    
+    
+    //definzione schema dataframe finale    
+    val schema = new StructType()
+      .add(StructField("fee", DoubleType, false, Metadata.empty))
+      .add(StructField("segwit", BooleanType, false, Metadata.empty))
+      .add(StructField("size", IntegerType, false, Metadata.empty))
+      .add(StructField("t", LongType, false, Metadata.empty))
+      .add(StructField("tfs", LongType, false, Metadata.empty))
+      .add(StructField("vol", DoubleType, false, Metadata.empty))
+      .add(StructField("hash", StringType, false, Metadata.empty))
+      .add(StructField("confTime", LongType, false, Metadata.empty))
+      .add(StructField("hourOfDay", LongType, false, Metadata.empty))
+      .add(StructField("price", DoubleType, false, Metadata.empty))
+      
+    //creazione e popolamento dataframe con esclusione righe contenenti campi NaN  
+    val df = spark.createDataFrame(rddJoined, schema).na.drop()
+    df.show()
+
+      //algoritmo di machine learning supervisionato per predizione tempo conferma Bitcoin mediante regressione lineare
+    //definizione vettore di features
+    val assembler = new VectorAssembler().setInputCols(Array("fee","segwit","size","t","tfs","vol","hourOfDay","price")).setOutputCol("features")
+    val dfML = assembler.transform(df).cache()
+    
+    
+    //definizione training e test set
+    val Array(training, test) = dfML.randomSplit(Array(0.7, 0.3), 123)
+    println("training count:"+training.count())
+    println("test count:"+test.count())
+
+    
+    //creazione modello
+    val lr = new LinearRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setFeaturesCol("features")
+      .setPredictionCol("predictedConfTime")
+      .setLabelCol("confTime")
+
+      
+    //apprendimento modello su training set
+    val lrModel = lr.fit(training)
+
+    
+    //stampa del coefficiente angolare e intercetta della funzione individuata
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    
+    //stampa statistiche
+    val trainingSummary = lrModel.summary
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")    
+    
+    
+    //applicazione modello su dati di test e valorizzazione predittori
+    println(new Date())
+    val predictions = lrModel.transform(test)    
+    println(new Date())
+    predictions.withColumn("diff", col("confTime")-col("predictedConfTime")).show
 //    predictions.show()
-//    var lstPreds = new ListBuffer[Array[String]]()
-//    
-//    
+    
+    
+    
 //    //estrazione csv predizioni
+//    var lstPreds = new ListBuffer[Array[String]]()
 //    lstPreds.append(Array("date", "price", "predictedPrice"))    
 //    predictions.sort("date").collect().foreach(r=>lstPreds.append(Array(r.get(0).toString(),decF.format(r.get(1)),decF.format(r.get(7)))))    
 //    writeCSV("web/csv/predictions.csv", lstPreds.toList)
+    
+    
     
     
     //terminazione contesto
