fb5f4d24befd3920f386d026ffce2a98ea33e5ea
RF <faro1976@a.a>
2019-10-14 11:58:33 +0200

    minor

--- thesis/TAPAS/bin/start-all.sh
+++ thesis/TAPAS/bin/start-all.sh
@@ -18,17 +18,19 @@ sbin/start-dfs.sh
 
 sleep 5
 #start spark
-cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7
 export SPARK_IDENT_STRING=spark1
+cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_1
 sbin/start-master.sh --port 8001 --webui-port 8011
 sleep 2
 sbin/start-slave.sh localhost:8001
 export SPARK_IDENT_STRING=spark2
+cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_2
 sleep 2
 sbin/start-master.sh --port 8002 --webui-port 8012
 sleep 2
 sbin/start-slave.sh localhost:8002
 export SPARK_IDENT_STRING=spark3
+cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_3
 sleep 2
 sbin/start-master.sh --port 8003 --webui-port 8013
 sleep 2
